9eed447134c0e7897cedbb8e304890fdfd778aa5
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2023-03-03 09:39:10 +0100

    Update Gebruiker.py

--- Gebruiker.py
+++ Gebruiker.py
@@ -21,34 +21,4 @@ class Gebruiker:
         self.id = id
         self.firstname = voornaam
         self.lastname = achternaam
-<<<<<<< HEAD
         self.emailadres = emailadres
-=======
-        self.emailadres = emailadres
-
-    def addReservation(self, reservatie):
-        """
-        Voegt de reservatie toe aan een gebruiker.
-
-        Preconditie: \
-
-        Postconditie: Gebruiker heeft een nieuwe reservatie.
-
-        :param reservatie: De reservatie die toegevoegd wordt aan de gebruiker.
-        :return: True als de operatie is gelukt, False als het niet gelukt is.
-        """
-        self.reservaties.enqueue(reservatie)
-
-    def removeReservation(self):
-        """
-        Verwijdert de eerst volgende reservatie van een gebruiker.
-
-        Preconditie: De gebruiker moet de reservatie hebben.
-
-        Postconditie: De reservatie van de persoon is verwijderd.
-
-        :param reservatie: De reservatie die wordt verwijderd van de gebruiker.
-        :return: True als de operatie is gelukt, False als het niet gelukt is.
-        """
-        self.reservaties.dequeue()
->>>>>>> 04b6d0d13713a0b0430bc9b66c748dbdd8aadb6e
