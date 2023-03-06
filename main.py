bde03d717ee8cba6330bd1f3e0971f26139e5fcb
SiBMs58 <siebe.mees@telenet.be>
2023-03-06 17:16:07 +0100

    Read inputFiles

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from Reservatiesysteem import Reservatiesysteem
-from movieWebScraper import getMovies
+#from movieWebScraper import getMovies
 
 def main():
     reservatieSysteem = Reservatiesysteem()
-
-    names, ratings = getMovies()
+    print("Hello main")
+    reservatieSysteem.readFile("Tests/Input/system.txt")
+    """names, ratings = getMovies()
     for name, rating in zip(names, ratings):
         reservatieSysteem.addMovie(name, rating)
-    reservatieSysteem.movies.inorderTraverse(print)
+    reservatieSysteem.movies.inorderTraverse(print)"""
+
+
+
 
 if __name__ == "__main__":
     main()
