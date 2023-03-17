97e18054eed1570d2a35889533d3a6fdb80babe6
SiBMs58 <siebe.mees@telenet.be>
2023-03-17 11:30:09 +0100

    BST Tabel

--- Datastructuren/SIEBE/Wrappers/BSTTable.py
+++ Datastructuren/SIEBE/Wrappers/BSTTable.py
@@ -1,31 +1,37 @@
-"""
-TABLE REQUIREMENTS
-vvvvvvvvvvvvvvvvvv
-"""
+from Datastructuren.SIEBE.Datatypes import MyBST, createTreeItem
 
-class ADT:
-    pass
-
-class Table(ADT):
+class BSTTable(MyBST):
     def __init__(self) -> None:
         super().__init__()
 
     #returns True if the ADT is empty
-    def tableIsEmpty():
-        pass
+    def tableIsEmpty(self):
+        return super().isEmpty()
 
     #Inserts an item into the ADT
-    def tableInsert(item):
-        pass
+    def tableInsert(self, item):
+        return super().searchTreeInsert(item)
 
     #Deletes an item from the ADT
-    def tableDelete(item):
-        pass
+    def tableDelete(item, key):
+        return super().searchTreeDelete(key)
 
     #Retrieves an item from the ADT
-    def tableRetrieve(key):
-        pass
+    def tableRetrieve(self, key):
+        return super().searchTreeRetrieve(key)
+
+    #InorderTraverses the table
+    def traverseTable(self, func):
+        super().inorderTraverse(func)
+
+    # Saves the table as a dict
+    def save(self):
+        return super().save()
+
+    # Loads the dict as a table
+    def load(self, input):
+        return super().load(input)
 
     #Creates an item that can be inserted into the ADT
-    def createItem(key, val):
-        pass
+    def createItem(self, key, val):
+        return createTreeItem(key, val)
