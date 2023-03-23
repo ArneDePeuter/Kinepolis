9a14200335ba8e93d09f9397632aaa3e7090e192
SiBMs58 <siebe.mees@telenet.be>
2023-03-23 22:54:47 +0100

    GebruikerTests.py

---
+++ Tests/GebruikerTests.py
@@ -0,0 +1,14 @@
+import unittest
+from Gebruiker import *
+
+class GebruikerTest(unittest.TestCase):
+    def test_init(self):
+        gebruiker = Gebruiker(555, "Siebe", "Mees", "siebe.mees@student.uantwerpen.be")
+        self.assertEqual(555, gebruiker.id)
+        self.assertEqual("Siebe", gebruiker.firstname)
+        self.assertEqual("Mees", gebruiker.lastname)
+        self.assertEqual("siebe.mees@student.uantwerpen.be", gebruiker.emailadres)
+
+
+if __name__ == '__main__':
+    unittest.main()
