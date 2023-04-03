0b1b5deff7414803d1c07791c58df99c4f09aa7d
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2023-04-03 18:25:14 +0200

    event.update added

--- kinepolis/Event.py
+++ kinepolis/Event.py
@@ -12,17 +12,7 @@ class EventSystem:
             return
         
         if top.timestamp <= self.system.clock:
-            if type(top)==Ticket:
-                self.system.getReservationSystem().useTicket(top.screeningId, top.seats)
-            elif type(top)==Reservation:
-                self.system.getReservationSystem().reservate(top.userId, top.screeningId, top.seats)
-                self.reservationCount -= 1
-            elif type(top)==Log:
-                self.system.save(top.filename)
-            elif type(top)==StartScreening:
-                screening, succes = self.system.getScreeningSystem().retrieve(top.screeningID)
-                if succes:
-                    screening.startScreening()
+            top.update(self)
             self.update()
         else:
             self.events.enqueue(top.timestamp, top)
@@ -79,12 +69,18 @@ class Ticket(Event):
         self.seats = seats
         str = f"Seats: {seats} - ScreeningId: {screeningId}"
         super().__init__(timestamp, "Ticket", str)
+    
+    def update(self, eventSysem):
+        eventSysem.system.getReservationSystem().useTicket(self.screeningId, self.seats)
 
 class Log(Event):
     def __init__(self, timestamp, filename) -> None:
         self.filename = filename
         str = f"Filename: {filename}"
         super().__init__(timestamp, "Log", str)
+    
+    def update(self, eventSysem):
+        eventSysem.system.save(self.filename)
 
 class Reservation(Event):
     def __init__(self, timestamp, id, userId, screeningId, seats) -> None:
@@ -96,9 +92,17 @@ class Reservation(Event):
         self.type = "Reservation"
         str = f"id:{id}, userId:{userId}, screeningId:{screeningId}, seats:{seats}"
         super().__init__(timestamp, "Reservation", str)
-
+    
+    def update(self, eventSysem):
+        eventSysem.system.getReservationSystem().reservate(self.userId, self.screeningId, self.seats)
+        eventSysem.reservationCount -= 1
 class StartScreening(Event):
     def __init__(self, timestamp, screeningId) -> None:
         self.screeningID = screeningId
         str = f"ScreeningId:{screeningId}"
         super().__init__(timestamp, "Start Screening", str)
+    
+    def update(self, eventSystem):
+        screening, succes = eventSystem.system.getScreeningSystem().retrieve(self.screeningID)
+        if succes:
+            screening.startScreening()
