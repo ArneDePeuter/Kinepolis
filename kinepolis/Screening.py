ae986da64cc4c867b47f2f32156de4a7e2ed769c
samds4 <samdesmet2004@gmail.com>
2023-04-18 14:18:57 +0200

    al een deel contracte geschreve voor screening

--- kinepolis/Screening.py
+++ kinepolis/Screening.py
@@ -7,14 +7,39 @@ from datetime import datetime, timedelta
 # Implementeren: Sam
 class ScreeningSystem:
     def __init__(self, system) -> None:
+        """
+        Initializes a ScreeningSystem
+
+        precondition : /
+        postcondition : a ScreeningSystem is created
+        """
         self.datastruct = ADTFactory.getADT("Screening")
         self.system = system
         self.count = 0
 
     def traverse(self, func):
+        """
+        Traverses through the datastructure used in the ScreeningSystem
+
+        :param func: function that gets executed on the objects
+        precondition : ScreeningSystem is initialized
+        postcondition : Input parameter function gets executed on objects
+        """
         self.datastruct.traverseTable(func)
 
     def addScreening(self, RoomNumber, slot, Date, filmsearchkey, FreePlaces, id=None):
+        """
+        Adds a screening to the ScreeningSystem
+
+        :param RoomNumber:
+        :param slot: The time slot of the screening
+        :param Date: The date of the screening
+        :param filmsearchkey: The searchkey of the movie of the screening
+        :param FreePlaces: The amount of freeplaces in the room
+        :param id: A unique number for that corresponds to the screening
+        precondition : ScreeningSystem is initialized
+        postcondition : A new screening is inserted into the ScreeningSystem
+        """
         if id is None:
             id = self.count
         else:
@@ -38,9 +63,26 @@ class ScreeningSystem:
         return True
 
     def retrieve(self, searchkey):
+        """
+        Retrieves a searchkey from the datastructure from the ScreeningSystem
+
+        :param searchkey: searchkey of a screening
+        precondition : ScreeningSystem is initialized
+        postcondition : returns object and true or none and false depending on whether the searchkey exists in the datastructure of the ScreeningSystem                         the searchkey exists inside of the datastructure
+        """
         return self.datastruct.tableRetrieve(searchkey)
 
     def query(self, searchkey, identifier):
+        """
+        Queries all the Movies corresponding to the searchkey and identifier
+
+        possible identifiers:
+            - "id"
+
+        :param searchkey: is the searchkey
+        :param identifier: is the identified param you want to search on
+        :return: returns a python list filled with items corresponding to the query
+        """
         d = {
             "id": MaterializedIndex(self.datastruct, Screening.getId),
             "roomNumber": MaterializedIndex(self.datastruct, Screening.getRoomNumber),
@@ -66,6 +108,17 @@ class ScreeningSystem:
 
 class Screening:
     def __init__(self, id, RoomNumber, timestamp, filmSearchkey, FreePlaces):
+        """
+        Initializes a Screening object
+
+        :param id:
+        :param RoomNumber:
+        :param timestamp:
+        :param filmSearchkey:
+        :param FreePlaces:
+        precondition:
+        postcondition:
+        """
         self.id = id
         self.roomNumber = RoomNumber
         self.timestamp = timestamp
