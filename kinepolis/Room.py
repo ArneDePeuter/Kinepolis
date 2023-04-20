0aecaee3ca14af2d2ad12cf5c69284e5f871dbf9
samds4 <samdesmet2004@gmail.com>
2023-04-20 23:09:02 +0200

    room contracten

--- kinepolis/Room.py
+++ kinepolis/Room.py
@@ -6,30 +6,55 @@ from .MaterializedIndex import MaterializedIndex
 # Implementeren: Arne
 class RoomSystem:
     def __init__(self) -> None:
+        """
+        Initializes a RoomSystem
+
+        Pre-condition : /
+        Post-condition : A RoomSystem is created
+        """
         self.datastruct = ADTFactory.getADT("Room")
         self.count = 0
 
     def traverse(self, func):
+        """
+         Traverses through the datastructure used in the RoomSystem
+
+         :param func: function that gets executed on the objects
+         precondition : RoomSystem is initialized
+         postcondition : Input parameter function gets executed on objects
+         """
         self.datastruct.traverseTable(func)
 
-    def addRoom(self, aantalPlaatsen, zaalNummer=None):
+    def addRoom(self, numberPlaces, Roomnumber=None):
         """
-        Voegt een zaal toe aan het reservatiesysteem.
-        Pre-condition: \
-        Post-condition: De zaal is toegevoegd aan het reservatiesysteem.
-        Maar niet als er al een zaal bestaat met dezelfde nummer.
-        :param zaalNummer: De zaal die wordt toegevoegd.
-        :param aantalPlaatsen: Het aantal plaatsen dat de zaal heeft.
-        :return: True als de operatie is gelukt, False als het niet gelukt is.
+        Add a room to the reservation system.
+
+        Pre-condition:
+        Post-condition: The room has been added to the reservation system.
+        But not if a room with the same number already exists.
+        :param Roomnumber: The room being added.
+        :param numberPlaces: The number of seats that the room has.
+        :return: True if the operation is successful, False if it fails.
         """
         if zaalNummer is not None:
-            self.count = max(self.count, zaalNummer)
+            self.count = max(self.count, Roomnumber)
 
-        newRoom = Room(self.count, aantalPlaatsen)
+        newRoom = Room(self.count, numberPlaces)
         self.datastruct.tableInsert(newRoom.roomNumber, newRoom)
         self.count += 1
 
     def query(self, searchkey, identifier):
+        """
+        Queries all the Rooms corresponding to the searchkey and identifier
+
+        possible identifiers:
+            - "amountOfSeats"
+            - "roomNumber"
+
+        :param searchkey: is the searchkey
+        :param identifier: is the identified param you want to search on
+        :return: returns a python list filled with items corresponding to the query
+        """
         d = {
             "roomNumber": MaterializedIndex(self.datastruct, Room.getRoomNumber),
             "amountOfSeats": MaterializedIndex(self.datastruct, Room.getAmountOfSeats),
@@ -41,22 +66,33 @@ class RoomSystem:
 
 
 class Room:
-    def __init__(self, zaalNummer, aantalPlaatsen):
+    def __init__(self, roomNumber, numberPlaces):
         """
-        Ceëert een zaal.
+        Create a room.
 
-        Pre-condition: Het aantalPlaatsen en zaalNummer zijn natuulrijke getallen.
+        Pre-condition: The numberOfSeats and roomNumber are natural numbers.
+        Post-condition: A room has been created.
 
-        Post-condition: Een zaal is aangemaakt.
-
-        :param zaalNummer: Het nummer van de zaal.
-        :param aantalPlaatsen: Het aantal plaatsen in de zaal.
+        :param roomNumber: The number of the room.
+        :param numberOfSeats: The number of seats in the room.
         """
-        self.amountOfSeats = aantalPlaatsen
-        self.roomNumber = zaalNummer
+        self.amountOfSeats = numberPlaces
+        self.roomNumber = roomNumber
 
     def getAmountOfSeats(self):
+        """
+        Returns amount of seats
+
+        Pre-condition: Room is initialized
+        Post-condition: Amount of seats is returned
+        """
         return self.amountOfSeats
 
     def getRoomNumber(self):
+        """
+        Returns roomnumber
+
+        Pre-condition: Room is initialized
+        Post-condition: Roomnumber is returned
+        """
         return self.roomNumber
