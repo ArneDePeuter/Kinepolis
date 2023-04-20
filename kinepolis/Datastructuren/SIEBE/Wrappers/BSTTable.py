521d626470a34ec0126ea63849ed828fbdd4dd73
SiBMs58 <siebe.mees@telenet.be>
2023-04-20 21:32:22 +0200

    BSTTable fix

--- kinepolis/Datastructuren/SIEBE/Wrappers/BSTTable.py
+++ kinepolis/Datastructuren/SIEBE/Wrappers/BSTTable.py
@@ -1,57 +1,35 @@
-#from ..Datatypes.BST import BST, Item
 from ..Datatypes.MyBST import BST, Node
 
-class BSTTable:
-    # Initialisation BSTTable
-    def __init__(self) -> None:
-        self.bst = BST()
+class BSTTable(BST):
+    #Initialisatie BST
+    def __init__(self):
+        super().__init__()
 
-    # Checks if the table is empty
-    def tableIsEmpty(self):
-        """
-        Checks if the table is empty
-        Pre-conditions: None
-        Post-conditions: Returns True if the table is empty
-        """
-        return self.bst.isEmpty()
-
-    # Inserts a TreeItem to the table
+    #Insert van een item
     def tableInsert(self, key, val):
-        """
-        Inserts a TreeItem to the table
-        TreeItem is of type twoThreeNode
-        Pre-conditions: None
-        Post-conditions: The treeItem gets inserted to the table
-        """
-        return self.bst.searchTreeInsert(Node(key, val))
+        return super().searchTreeInsert(Node(key,val))
 
-    # Retrieves an item from the table
-    def tableRetrieve(self, key):
-        """
-        Retrieves an item from the table
-        Pre-conditions: None
-        Post-conditions: Returns a tuple (retrievedItem = item/None, done = True/False)
-        """
-        item, succes = self.bst.searchTreeRetrieve(key)
-        if succes:
-            return item.item, succes
-        return False, None
-
-    # Prints the table inorder
-    def traverseTable(self, func):
-        """
-        Prints the table inorder
-        Pre-conditions: None
-        Post-conditions: prints the table inorder
-        """
-        self.bst.inorderTraverse(func)
-
-    # Deletes an item from the table
+    #Delete van een item
     def tableDelete(self, key):
-        """
-        Deletes an item from the table
-        item is the target for deletion
-        Pre-conditions: None
-        Post-conditions: The given item gets deleted from the table
-        """
-        return self.bst.searchTreeDelete(item)
+        return super().searchTreeDelete(key)
+
+    #Checkt of table leeg is
+    def tableIsEmpty(self):
+        return super().isEmpty()
+
+    #Traverse van de tabel doen
+    def traverseTable(self, func):
+        super().inorderTraverse(func)
+
+    #Retrieved object of waarde door op key te zoeken.
+    def tableRetrieve(self, key):
+        return super().searchTreeRetrieve(key)
+
+    #Print de boom uit
+    def save(self):
+        return super().save()
+
+    #Load een boom vanuit output van save
+    def load(self, input):
+        return super().load(input)
+
