24c9c75af7ad179c750d5a500e5aeeecc995fcbb
samds4 <samdesmet2004@gmail.com>
2023-04-20 23:24:58 +0200

    outputter contracts

--- kinepolis/Outputter.py
+++ kinepolis/Outputter.py
@@ -5,9 +5,22 @@ PATH = "./Output/"
 
 class Outputter:
     def __init__(self, system) -> None:
+        """
+        Initializes an outputter
+
+        :param system: Kinepolis object
+        Pre-condition: /
+        Post-condition: Creates an ouputter system
+        """
         self.system = system
 
     def getLog(self):
+        """
+        Returns the log
+
+        Pre-condition : Outputter is initialized
+        Post-condition : Returns the log of the system
+        """
         with open("./kinepolis/template.html", "r") as template_file:
             template = Template(template_file.read())
 
@@ -29,6 +42,13 @@ class Outputter:
         return template.render(screenings=screenings, events=events, time=time)
 
     def generate(self, filename):
+        """
+        Generates the log into a html file
+
+        :param filename: Name of the file
+        Pre-condition : Outputter is initialized
+        Post-condition : Output file is generated
+        """
         f = open(PATH + filename, "w")
 
         html = f"""<!DOCTYPE html>
